8425e6a142a7b87df809c849dec5f6e803e9bd5e
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-07-13 09:42:12 +0530

    Initial Commit

---
+++ src/main/java/com/mysql/depemp/DepartmentEmployeeAppMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.mysql.depemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentEmployeeAppMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentEmployeeAppMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysql/depemp/DepartmentEmployeeAppMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysql.depemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentEmployeeAppMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
