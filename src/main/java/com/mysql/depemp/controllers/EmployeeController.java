f0e1a212ad962aa3fd236c1c4f474a32200509a2
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-07-15 14:17:29 +0530

    Added DELETE Rest API for EmployeeController

--- src/main/java/com/mysql/depemp/controllers/EmployeeController.java
+++ src/main/java/com/mysql/depemp/controllers/EmployeeController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,5 +45,13 @@ public class EmployeeController {
 	{
 		empRepo.save(emp);
 		return emp;
+	}
+	
+	@DeleteMapping("/delete/{empId}")
+	public Employee deleteEmployee(@PathVariable int empId)
+	{
+		Employee newEmp = (Employee)empRepo.findById(empId).orElse(new Employee());
+		empRepo.deleteById(empId);;
+		return newEmp;
 	}
 }
